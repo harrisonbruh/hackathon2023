e08904cc9c11a3519ea11e3ad5d0b11c793a8bec
Kristen Heller <kaheller@wpi.edu>
2023-01-14 12:41:20 -0500

    Activity lifecycle methods outline plus a tad more bluetooth

--- app/src/main/java/com/example/catapp/MainActivity.java
+++ app/src/main/java/com/example/catapp/MainActivity.java
@@ -8,9 +8,42 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
     BluetoothSocket phone;
     BluetoothDevice catBot;
+    BluetoothAdapter adapter; //needed to find catbot
+    BluetoothServerSocket serverSocket; // needed to set up BluetoothSocket phone
+
+    //Android App Lifecycle Methods: onCreate, onStart, onResume, onPause, onStop, onDestroy
+    //https://developer.android.com/reference/android/app/Activity.html#ActivityLifecycle
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+
 }
