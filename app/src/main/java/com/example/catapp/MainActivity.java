6577ba3c69ff841d1d72b11ca2b466b5b7a28468
Kristen Heller <kaheller@wpi.edu>
2023-01-14 13:59:02 -0500

    Bluetooth Additions

--- app/src/main/java/com/example/catapp/MainActivity.java
+++ app/src/main/java/com/example/catapp/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.catapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.Activity;
 import android.bluetooth.*;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+
+import java.util.*;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     BluetoothDevice catBot;
     BluetoothAdapter adapter; //needed to find catbot
     BluetoothServerSocket serverSocket; // needed to set up BluetoothSocket phone
+    BluetoothManager manager;
 
     //Android App Lifecycle Methods: onCreate, onStart, onResume, onPause, onStop, onDestroy
     //https://developer.android.com/reference/android/app/Activity.html#ActivityLifecycle
@@ -18,6 +26,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //unsure on casting, assuming this is a subclass of Context
+        manager = (BluetoothManager) this.getSystemService(BLUETOOTH_SERVICE);
+
+        //get BluetoothAdapter from BluetoothManager
+        adapter = manager.getAdapter();
+
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 1);
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Set<BluetoothDevice> devices = adapter.getBondedDevices();
+
+
     }
 
     @Override
@@ -46,7 +77,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy(){
         super.onDestroy();
     }
+    //Bluetooth setup methods
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 
+
+    //Button Press methods
     public void upClick(View view) {
     }
 
@@ -59,6 +97,5 @@ public class MainActivity extends AppCompatActivity {
     public void rightClick(View view) {
     }
 
-    // Button Press methods
 
 }
